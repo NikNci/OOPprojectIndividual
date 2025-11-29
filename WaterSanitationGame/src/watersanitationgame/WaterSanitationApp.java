a6f25b08a8ba298f7700ef117eb46ecdcaf2670f
NikNci <x24465532@student.ncirl.ie>
2025-11-29 12:32:15 +0000

    first

---
+++ WaterSanitationGame/src/watersanitationgame/WaterSanitationApp.java
@@ -0,0 +1,17 @@
+package watersanitationgame;
+
+/**
+ * WaterSanitationApp.java for individual checkpoint
+ * @authors Nikolas Misins
+ */
+
+//imports
+import  watersanitationgame.Events_Nikolas.EventsGUI;
+
+public class WaterSanitationApp {
+    public static void main(String[] args) {
+        //Start first GUI
+        EventsGUI w = new EventsGUI();
+        w.setVisible(true);
+    }
+}

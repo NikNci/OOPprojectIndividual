624a7992a38e1e1b55b176027b3f1d8042389f93
NikNci <x24465532@student.ncirl.ie>
2025-11-30 16:15:38 +0000

    Add files via upload

--- WaterSanitationGame/src/watersanitationgame/WaterSanitationApp.java
+++ WaterSanitationGame/src/watersanitationgame/WaterSanitationApp.java
@@ -10,8 +10,8 @@ import  watersanitationgame.Events_Nikolas.EventsGUI;
 
 public class WaterSanitationApp {
     public static void main(String[] args) {
-        //Start first GUI
-        EventsGUI w = new EventsGUI();
+        //for the purpose of  the individual checkpoint submission, the app class immediately opens my own section
+        EventsGUI w = new EventsGUI(0);
         w.setVisible(true);
     }
 }
